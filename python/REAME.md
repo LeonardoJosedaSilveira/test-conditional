42249ad15a4e9ece32d0659a30715d040a62c076
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-09-23 18:22:51 -0300

    ex1 range multeply

---
+++ python/REAME.md
@@ -0,0 +1,5 @@
+Repositorio fonte dos exercicios.
+
+https://github.com/zhiwehu/Python-programming-exercises/blob/master/100%2B%20Python%20challenging%20programming%20exercises%20for%20Python%203.md
+
+https://github.com/zhiwehu
---
+++ python/range_lv1.py
@@ -0,0 +1,17 @@
+'''
+Questão 1
+Nível 1
+
+Pergunta: Escreva um programa que encontre todos os números divisíveis por 7,
+mas não múltiplos de 5, entre 2000 e 3200 (ambos incluídos).
+Os números obtidos devem ser impressos em seqüência separada por vírgulas em uma única linha.
+
+'''
+
+list = set()
+
+for item in range(2000, 3201):
+    if (item % 7) == 0 and (item % 5) != 0:
+        list.add(item)
+
+print(sorted(list) )
