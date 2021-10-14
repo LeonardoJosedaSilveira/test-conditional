a4256a9ab405a9d1222cd6667a7f4cd47bd6f453
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-10-13 22:17:08 -0300

    lv2 quest7 and test

---
+++ python/src/lv2/matriz.py
@@ -0,0 +1,29 @@
+'''
+Level 2
+Question 7
+
+Pergunta: Escreva um programa que tenha 2 dígitos, X, Y como entrada 
+e gere um array bidimensional. 
+O valor do elemento na i-ésima linha e j-ésima coluna da matriz 
+deve ser i * j. Nota: i = 0,1 .., X-1; j = 0,1, ¡Y-1. 
+Exemplo Suponha que as seguintes entradas sejam fornecidas ao 
+programa: 3,5 Então, a saída do programa deve ser: [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [ 0, 2, 4, 6, 8]]
+
+Dicas: Nota: No caso de dados de entrada sendo fornecidos para a questão, 
+deve ser assumido como uma entrada de console em um formato separado por vírgulas.
+'''
+
+
+def calc_create_line(x, y):
+    line = []
+    for collum in range(x):
+        line.append(y*collum)
+    return line
+
+
+def calc_and_create_matriz(x, y):
+    matriz = []
+    for line in range(x):
+        matriz.append(calc_create_line(y, line))
+
+    return matriz
--- python/tests/lv2/test_matriz.py
+++ python/tests/lv2/test_matriz.py
@@ -5,4 +5,4 @@ def test_calc_and_create_matriz():
     results_exp = [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
     result = calc_and_create_matriz(3, 5)
 
-    assert result == results_exp is True
+    assert (result == results_exp) is True
