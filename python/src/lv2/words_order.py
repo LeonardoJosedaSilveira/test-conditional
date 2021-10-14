2108e01af9a08d29168dcb93f37db9609932f826
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-10-14 00:35:30 -0300

    corret flack 8

--- python/src/lv2/words_order.py
+++ python/src/lv2/words_order.py
@@ -2,13 +2,14 @@
 Level 2
 Question 8
 
-Pergunta: Escreva um programa que aceite uma sequência de palavras 
-separadas por vírgulas como entrada e imprima as palavras em uma 
-sequência separada por vírgulas após classificá-las em ordem alfabética. 
-Suponha que a seguinte entrada seja fornecida ao programa: without, hello, 
+Pergunta: Escreva um programa que aceite uma sequência de palavras
+separadas por vírgulas como entrada e imprima as palavras em uma
+sequência separada por vírgulas após classificá-las em ordem alfabética.
+Suponha que a seguinte entrada seja fornecida ao programa: without, hello,
 bag, world Então, a saída deve ser: bag, hello, without, world
 
-Dicas: No caso de dados de entrada fornecidos para a pergunta, deve ser considerada uma entrada do console.
+Dicas: No caso de dados de entrada fornecidos para a pergunta,
+deve ser considerada uma entrada do console.
 '''
 
 
@@ -22,5 +23,5 @@ def words_order(words):
             result = word.strip()
         else:
             result += ', {}'.format(word.strip())
-    
+
     return result
